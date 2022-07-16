b7d00445ce0ddb03e4ea0ef27ea8860c2f2eb260
Alex Verstraten <alexverstraten03@gmail.com>
2022-07-16 16:08:32 +0200

    spawning characeters

--- Assets/Scripts/PointScript.cs
+++ Assets/Scripts/PointScript.cs
@@ -1,26 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PointScript : MonoBehaviour
 {
     [SerializeField] private int currentAmountPoints;
+    [SerializeField] private TextMeshProUGUI pointsText;
 
     public int CurrentAmountPoints { get { return currentAmountPoints; } set { currentAmountPoints = value; } }
+    public DiceThrower DiceThrower { get { return diceThrower; } }
 
     private GameManager gameManager;
+    private DiceThrower diceThrower;
 
     private void Start()
     {
         gameManager = GameManager.Instance;
+        diceThrower = GetComponent<DiceThrower>();
     }
 
     public void UsePoint(int amount)
     {
         if (CurrentAmountPoints > 0 && amount <= CurrentAmountPoints)
+        {
             CurrentAmountPoints -= amount;
-        else
-            Debug.Log("No points");//Show that you have no points left
+            if (pointsText)
+                pointsText.text = CurrentAmountPoints.ToString();
+
+            if(CurrentAmountPoints <= 0)
+            {
+                gameManager.NextTurn();
+                diceThrower.isTurn = false;
+                Debug.Log("No points");//Show that you have no points left
+            }
+        }
     }
 
     public void GetPoints()
