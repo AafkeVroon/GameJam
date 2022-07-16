741739ed7111ca392f937ef9bd67ca3db5123fe4
Alex Verstraten <alexverstraten03@gmail.com>
2022-07-16 08:36:55 +0200

    movement smooth

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,12 +4,18 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] private float desiredWalkTime = 2;
+
     private PointScript pointScript;
     private bool canMove;
+    private Animator anim;
+    private Vector3 nextPosition;
+    private float elapsidedTime;
 
     private void Start()
     {
         pointScript = GetComponent<PointScript>();
+        anim = GetComponent<Animator>();
         canMove = true;
     }
 
@@ -24,23 +30,35 @@ public class PlayerMovement : MonoBehaviour
         {
             if(Input.GetKeyDown(KeyCode.W) && canMove)
             {
-                transform.position += new Vector3(0, 0, 1);
+                nextPosition = transform.position += new Vector3(0, 0, 2);
+                transform.rotation = Quaternion.LookRotation(Vector3.forward);
                 UseAction();
             }
             if (Input.GetKeyDown(KeyCode.S) && canMove)
             {
-                transform.position += new Vector3(0, 0, -1);
+                nextPosition = transform.position += new Vector3(0, 0, -2);
+                transform.rotation = Quaternion.LookRotation(Vector3.back);
                 UseAction();
             }
             if (Input.GetKeyDown(KeyCode.D) && canMove)
             {
-                transform.position += new Vector3(1, 0, 0);
+                nextPosition = transform.position += new Vector3(2, 0, 0);
+                transform.rotation = Quaternion.LookRotation(Vector3.right);
                 UseAction();
             }
             if (Input.GetKeyDown(KeyCode.A) && canMove)
             {
-                transform.position += new Vector3(-1, 0, 0);
+                nextPosition = transform.position += new Vector3(-2, 0, 0);
+                transform.rotation = Quaternion.LookRotation(Vector3.left);
                 UseAction();
+            }
+
+            if (!canMove)
+            {
+                elapsidedTime += Time.deltaTime;
+                float percentage = elapsidedTime / desiredWalkTime;
+                Debug.Log(percentage);
+                transform.position = Vector3.Lerp(transform.position, nextPosition, percentage);
             }
         }
     }
@@ -54,7 +72,7 @@ public class PlayerMovement : MonoBehaviour
 
     private IEnumerator SetCanMoveToTrue()
     {
-        yield return new WaitForSeconds(1);
+        yield return new WaitForSeconds(desiredWalkTime + 0.05f);
         canMove = true;
     }
 }
